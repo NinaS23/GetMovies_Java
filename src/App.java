5a9b2b54033c83f8ffaa2f71e1358d2b5f754071
DUDA <mariabevenuto062@gmail.com>
2023-03-27 12:22:57 -0300

    feat: style terminal

--- src/App.java
+++ src/App.java
@@ -6,6 +6,7 @@ import java.net.http.HttpResponse.BodyHandlers;
 import java.util.List;
 import java.util.Map;
 
+
 public class App {
 
     public static void main(String[] args) throws Exception {
@@ -18,10 +19,13 @@ public class App {
 
         JsonParse parser = new JsonParse();
         List<Map<String, String>> movieList = parser.parse(body);
-        for(Map<String, String> movie: movieList){
-            System.out.println(movie.get("title"));
-            System.out.println(movie.get("image"));
-            System.out.println(movie.get("imDbRating"));
+     
+
+        for(int i = 0; i < movieList.size(); i++){
+            Map<String,String> movie = movieList.get(i);
+            System.out.println(" title: " + movie.get("title"));
+            System.out.println(" image: " + movie.get("image"));
+            System.out.println(" avaliationValue: " + movie.get("imDbRating") +  " ⭐" );
         }
     }
 }
