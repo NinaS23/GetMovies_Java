1c6e5e889eb897490efc22e5a03a1d16ecc3ac2b
DUDA <mariabevenuto062@gmail.com>
2023-03-27 11:48:10 -0300

    feat: filter movies atributes

--- src/App.java
+++ src/App.java
@@ -3,15 +3,25 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
 
 public class App {
+
     public static void main(String[] args) throws Exception {
-       String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
-       URI adress = URI.create(url);
-       HttpClient client = HttpClient.newHttpClient();
-       HttpRequest request = HttpRequest.newBuilder(adress).GET().build();
-       HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-       String json = response.body();
-       System.out.println(json);
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI adress = URI.create(url);
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder(adress).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        JsonParse parser = new JsonParse();
+        List<Map<String, String>> movieList = parser.parse(body);
+        for(Map<String, String> movie: movieList){
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+        }
     }
 }
